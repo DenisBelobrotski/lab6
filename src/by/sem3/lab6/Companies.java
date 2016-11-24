e5f880b00f5bc188ca7029b93e3c6ca5180dd7b3
Denis Belobrotski <belobrotskiy@gmail.com>
2016-11-24 18:40:14 +0300

    second commit

--- src/by/sem3/lab6/Companies.java
+++ src/by/sem3/lab6/Companies.java
@@ -1,10 +1,9 @@
 package by.sem3.lab6;
 
 import java.io.PrintStream;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class Companies {
     private List<Company> list;
@@ -74,8 +73,66 @@ class Companies {
             }
         }
         if (shortTitle.size() == 0) {
-            ps.println("Companies with this short title aren't found!\n");
+            ps.println("Companies with short title aren't found!\n");
         }
         return shortTitle;
     }
+
+    public List<Company> findBranch(String str, PrintStream ps) {
+        List<Company> branch = new ArrayList<>();
+        for (Company item : list) {
+            if (str.equalsIgnoreCase(item.getBranch())) {
+                branch.add(item);
+            }
+        }
+        if (branch.size() == 0) {
+            ps.println("Companies of this branch aren't found!\n");
+        }
+        return branch;
+    }
+
+    public List<Company> findActivity(String str, PrintStream ps) {
+        List<Company> activity = new ArrayList<>();
+        for (Company item : list) {
+            if (str.equalsIgnoreCase(item.getBranch())) {
+                activity.add(item);
+            }
+        }
+        if (activity.size() == 0) {
+            ps.println("Companies of this activity aren't found!\n");
+        }
+        return activity;
+    }
+
+    public List<Company> findDateOfFoundation(LocalDate fromDate, LocalDate toDate, PrintStream ps) {
+        List<Company> dateOfFoundation = new ArrayList<>();
+        for (Company item : list) {
+            LocalDate date = item.getDateFoundation();
+            if (date.isAfter(fromDate) && date.isBefore(toDate)) {
+                dateOfFoundation.add(item);
+            }
+        }
+        if (dateOfFoundation.size() == 0) {
+            ps.println("Companies of this period of foundation aren't found!\n");
+        }
+        return dateOfFoundation;
+    }
+
+    public List<Company> findCountOfEmployees(int fromCount, int toCount, PrintStream ps) throws InputException {
+        if (fromCount < 0 || toCount < 0) {
+            throw new InputException("Wrong edges of employees count!\n");
+        }
+        List<Company> countOfEmployees = new ArrayList<Company>();
+        for (Company item : list) {
+            int count = item.getCountEmployees();
+            if (count >= fromCount && count <= toCount) {
+                countOfEmployees.add(item);
+            }
+        }
+        if (countOfEmployees.size() == 0) {
+            ps.println("Companies with this edges of employees count aren't found!");
+        }
+        return countOfEmployees;
+    }
+
 }
--- src/by/sem3/lab6/ProcessLogger.java
+++ src/by/sem3/lab6/ProcessLogger.java
@@ -11,6 +11,15 @@ class ProcessLogger {
             run.outputJSON();
             run.printSearchResultOfShortTitle("StiTlE3");
             run.printSearchResultOfShortTitle("sTitle1");
+            run.printSearchResultOfActivity("ACTIVITY3");
+            run.printSearchResultOfActivity("activity0");
+            run.printSearchResultOfBranch("braNCH1");
+            run.printSearchResultOfBranch("branch5");
+            run.printSearchResultOfDateOfFoundation(Check.checkDate("01/01/2015", true), Check.checkDate("01/01/2016", true));
+            run.printSearchResultOfDateOfFoundation(Check.checkDate("01/01/2000", true), Check.checkDate("01/01/2004", true));
+            run.printSearchResultOfCountOfEmployees(100, 1200);
+            run.printSearchResultOfCountOfEmployees(2, 10);
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         } catch (InputException e) {
